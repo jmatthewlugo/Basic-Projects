6c2f660b42ec714452e305a448de4fa6f9c332ed
jmatthewlugo <45877807+jmatthewlugo@users.noreply.github.com>
2019-09-11 20:45:31 -0500

    Update Animal_Crossing.py

--- Animal_Crossing.py
+++ Animal_Crossing.py
@@ -1,16 +1,8 @@
 import webbrowser, os
-from datetime import datetime
-import pytz
-from tzlocal import get_localzone
+import datetime as dt
 
-utc_dt = datetime(2009, 7, 10, 18, 44, 59, 193982, tzinfo=pytz.utc)
-current_time = utc_dt.astimezone(get_localzone())
-local_hour = current_time.hour
-print(utc_dt)
-print(current_time)
-
-## LOCAL_TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-## that is local timezone in dt
+current_time = dt.datetime.now()
+current_hour = current_time.hour
 
 embed_codes_nl = ['ogZOuyEUaqM','6ctYQ2giDfw','rjoi8HzYjnY','alCXAH7tzT4','YsRHAojQK20',
 'tnlpjqXHQnI','DHskPDrg-fQ','r7Ncy-yY9Uw','qRAt72y7Dl0','BtxRvNYYUUo','1ZXwbqjoHQ8','_4G7EWJ7Ik8',
@@ -22,43 +14,46 @@ new_leaf_tp = dict(zip(hours,embed_codes_nl))
 
 def video_link(hour):
     value = new_leaf_tp[hour]
-    link = 'https://www.youtube.com/embed/{}'.format(value)
+    link = 'https://www.youtube.com/embed/{}?'.format(value)
+    print(value)
     return link
 
-#youtube_link = video_link(18)
-
-
 def create_page(video_url):
-    os.chdir(r'C:\Users\Matthew\Desktop')
     filename = 'helloworld.html'
     f = open(filename,'w+')
-    message = """<!DOCTYPE html>
+    message = """<!DOCTYPE html5>
     <html>
-    This is where some of the text might go 
     <head>
-    That is
+
+    <title> Name of page here I guess </title>
     </head>
-
-    If I am actually correct
-
     <body>
-    <iframe width="420" height="345" src={URL}>
+ 
+    <iframe width="420" height="315" src={URL}>
     </iframe>
+    
+    <p>
+    <h1>If I am actually correct</h1>
+    </p>
 
-    </p></body>
-    A little text down here 
-
+    <p2>
+    <h2>A little text down here</h2>
+    </p2>
+    
+    <p3>
+    <h3>Finish it up down here</h3>
+    </p3>
+    
     </body>
-
-    Finish it up down here
     </html>"""
+
     complete_message = message.format(URL=video_url)
     f.write(complete_message)
     f.close()
-    return
+    page = webbrowser.open_new_tab(filename)
+    return page
 
-url = video_link(local_hour)
+url = video_link(current_hour)
+post = create_page(url)
+
 print(url)
-create_page(url)
-
-# webbrowser.open_new_tab(youtube_link)
